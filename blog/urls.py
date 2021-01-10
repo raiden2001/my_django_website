dcc815da14c59e598805be8692744a2059f596a1
Jason ma <jasonmengma08@gmail.com>
2021-01-10 17:24:20 -0500

    Add detail view for each blog post.

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # 127.0.0.1:8000 --> local
+    #mydjangosite.com -- > final app online
+
     # 127.0.01.:8000 it will take u too the post_list.html
-    path('', views.post_list, name='post_list'), # it will go here post_list.html
+    path('', views.post_list, name='post_list'),# it will go here post_list.html
+
+    #127.0.0.1:8000/post/2 --> local for primary Key
+    # mydangosite.com/post/2 === online
+
+    #127.0.0.1:8000/post/2 <int:pk> <------taht replaced or passed by 2 = pk replaced ,which has primary key of it indicates in the post
+    path('post/<int:pk>/', views.post_detail, name='post_detail')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
@@ -11,3 +11,13 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     stuff_for_frontend = {'posts': posts}  # posts is value where to u will store in the html in for loop
     return render(request, 'blog/post_list.html', stuff_for_frontend)
+
+
+    # from the urlspy the pk:2 is going to pass to this function here
+def post_detail(request, pk):  # as soon as you click on page
+    # u need the primary key to access the site
+    post = get_object_or_404(Post, pk=pk)
+    # Post will set as pk
+    # if its ges post as primary key or else gives error 404
+    stuff_for_frontend = {'post': post}
+    return render(request, 'blog/post_detail.html', stuff_for_frontend)
