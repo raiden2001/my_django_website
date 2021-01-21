e3744488586c44010ebb8510357f527573bbe08d
Jason ma <jasonmengma08@gmail.com>
2021-01-21 16:34:55 -0500

    Add basic authentic and icon to make new posts.

--- blog/forms.py
+++ blog/forms.py
@@ -6,4 +6,4 @@ class PostForm(forms.ModelForm):
     class Meta:  # give what kind of form
         model = Post
         # which fields want u to create
-        fields = ( 'title', 'text')
+        fields = ('title', 'text')
